f4daf447c6370517c60b91aa3547c772103d7531
Mohit Chandel <devmohit93@gmail.com>
2023-08-27 20:39:11 +0530

    Fix: created folder structure

---
+++ contract/target/debug/deps/contract-17d5982af74901ae.d
@@ -0,0 +1,5 @@
+/Users/mohitchandel/Documents/near-social-app/contract/target/debug/deps/contract-17d5982af74901ae.rmeta: src/main.rs
+
+/Users/mohitchandel/Documents/near-social-app/contract/target/debug/deps/contract-17d5982af74901ae.d: src/main.rs
+
+src/main.rs:
---
+++ contract/target/debug/deps/contract-92eb62d81f7cc790.d
@@ -0,0 +1,5 @@
+/Users/mohitchandel/Documents/near-social-app/contract/target/debug/deps/contract-92eb62d81f7cc790.rmeta: src/main.rs
+
+/Users/mohitchandel/Documents/near-social-app/contract/target/debug/deps/contract-92eb62d81f7cc790.d: src/main.rs
+
+src/main.rs:
