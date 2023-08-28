b0728f8bc355565745fa4c6127ede5de017422d5
Mohit Chandel <devmohit93@gmail.com>
2023-08-28 16:07:27 +0530

    Fix: some left overs

--- frontend/src/app/post/[postId]/page.tsx
+++ frontend/src/app/post/[postId]/page.tsx
@@ -3,11 +3,14 @@ import CommentSection from "@/components/CommentSection";
 import NavigationBar from "@/components/NavigationBar";
 import { faHeart } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
 import { useParams } from "next/navigation";
 
 export default function postPage() {
   const params = useParams();
   const postId = params.uid;
+
+  const dummyTags = ["tag1", "tag2", "tag3"];
 
   return (
     <>
@@ -35,6 +38,17 @@ export default function postPage() {
                 Here are the biggest enterprise technology acquisitions of 2021
                 so far, in reverse chronological order.
               </p>
+              <div className="grid grid-cols-12 gap-2 py-2">
+                {dummyTags.map((tag, index) => (
+                  <Link
+                    key={index}
+                    href={`/tag/${tag}`}
+                    className="px-2 py-1 text-sm bg-blue-500 text-white rounded-full hover:bg-blue-600"
+                  >
+                    {tag}
+                  </Link>
+                ))}
+              </div>
               <div>
                 <button>
                   <FontAwesomeIcon
@@ -42,7 +56,7 @@ export default function postPage() {
                     icon={faHeart}
                     size="1x"
                   />
-                  12
+                  {" 12"}
                 </button>
               </div>
             </div>
